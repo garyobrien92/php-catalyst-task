c803da02f3ffd6c33b6552295d7fe5902e42fb2a
Gary O Brien <garyobrieneu@gmail.com>
2024-03-29 00:00:54 +1100

    add menu options and users csv.

--- user_upload.php
+++ user_upload.php
@@ -1,9 +1,33 @@
 <?php
 
 $short_options = "";
-$long_options = ["file:", "create-table"];
+$long_options = ["file:", "create-table::", "help::"];
 $options = getopt($short_options, $long_options);
 $file = $options["file"];
+$createTable = array_key_exists("create-table", $options);
+
+$help = array_key_exists("help", $options);
+
+if($help) {
+    echo "\r\n";
+
+    echo "--file [csv file name] – this is the name of the CSV to be parsed" . "\r\n";
+
+    echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)" . "\r\n";
+
+    echo "--dry_run – this will be used with the --file directive in case we want to run the script but not insert
+    into the DB. All other functions will be executed, but the database won't be altered" . "\r\n";
+
+    echo "-u – MySQL username" . "\r\n";
+
+    echo "-p – MySQL password" . "\r\n";
+
+    echo "-h – MySQL host" . "\r\n";
+
+    echo "\r\n";
+
+    exit(1);
+}
 
 if(!isset($file) || empty(trim($file)) ) {
     echo "Please provide a file path for csv file";
@@ -15,8 +39,8 @@ if(!str_ends_with($file,".csv")) {
     return;
 }
 
-if(isset($file)) {
-    echo $file;
+if($createTable) {
+    echo "Create table fresh";
 }
 
 // include 'db.php';
