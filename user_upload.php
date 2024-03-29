23c67804853a80ad3af8283c8e86768b035559bf
Gary O Brien <garyobrieneu@gmail.com>
2024-03-29 18:53:30 +1100

    fix options inputs and clean up

--- user_upload.php
+++ user_upload.php
@@ -1,50 +1,73 @@
+#!/usr/bin/php
+
 <?php
+include 'db.php';
 
-$short_options = "";
-$long_options = ["file:", "create-table::", "help::"];
+$short_options = "u:p:h:";
+$long_options = ["file:", "create-table::", "dry-run::", "help::"];
 $options = getopt($short_options, $long_options);
-$file = $options["file"];
 $createTable = array_key_exists("create-table", $options);
-
+$dryRun = array_key_exists("dry-run", $options);
 $help = array_key_exists("help", $options);
+
+if(!isset($options["file"]) || empty(trim($options["file"])) ) {
+    echo "Please provide a file path for csv file";
+    return;
+}
+
+if(!str_ends_with($options["file"],".csv")) {
+    echo "Please provide a valid csv file";
+    return;
+}
+
+if(!isset($options["u"]) || empty(trim($options["u"])) ) {
+    echo "Please username for database";
+    return;
+}
+
+if(!isset($options["p"]) || empty(trim($options["p"])) ) {
+    echo "Please password for database";
+    return;
+}
+
+if(!isset($options["h"]) || empty(trim($options["h"])) ) {
+    echo "Please host for database";
+    return;
+}
 
 if($help) {
     echo "\r\n";
 
-    echo "--file [csv file name] – this is the name of the CSV to be parsed" . "\r\n";
+    echo "--file [csv file name] – this is the name of the CSV to be parsed" . "\n";
 
-    echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)" . "\r\n";
+    echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)" . "\n";
 
     echo "--dry_run – this will be used with the --file directive in case we want to run the script but not insert
-    into the DB. All other functions will be executed, but the database won't be altered" . "\r\n";
+    into the DB. All other functions will be executed, but the database won't be altered" . "\n";
 
-    echo "-u – MySQL username" . "\r\n";
+    echo "-u – MySQL username" . "\n";
 
-    echo "-p – MySQL password" . "\r\n";
+    echo "-p – MySQL password" . "\n";
 
-    echo "-h – MySQL host" . "\r\n";
+    echo "-h – MySQL host" . "\n";
 
     echo "\r\n";
 
     exit(1);
 }
 
-if(!isset($file) || empty(trim($file)) ) {
-    echo "Please provide a file path for csv file";
-    return;
-}
+var_dump($options);
 
-if(!str_ends_with($file,".csv")) {
-    echo "Please provide a valid csv file";
-    return;
-}
+$dbInstance = DatabaseConnect::getInstance( $options["h"], $options["u"], $options["p"], "users_csv_upload");
+$db = $dbInstance->getConnection();
+
+var_dump($db);
 
 if($createTable) {
     echo "Create table fresh";
 }
 
-// include 'db.php';
-
-// $dbInstance = DatabaseConnect::getInstance();
-// $db = $dbInstance->getConnection();
+if($dryRun) {
+    echo "Dry run";
+}
 ?>
