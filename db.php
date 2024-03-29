9b60d1f0c244e2c06147d3dc2318ed31883a02aa
Gary O Brien <garyobrien@Garys-MacBook-Pro-2.local>
2024-03-29 21:05:06 +1100

    Clean up and add insert query with validation.

--- db.php
+++ db.php
@@ -26,20 +26,4 @@ class DatabaseConnect {
         return $this->conn;
     }
 }
- 
-// Attempt create table query execution
-$sql = "CREATE TABLE persons(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(70) NOT NULL UNIQUE
-)";
-if($mysqli->query($sql) === true){
-    echo "Table created successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
-}
- 
-// Close connection
-$mysqli->close();
 ?>
--- user_upload.php
+++ user_upload.php
@@ -10,6 +10,25 @@ $createTable = array_key_exists("create-table", $options);
 $dryRun = array_key_exists("dry-run", $options);
 $help = array_key_exists("help", $options);
 
+if($help) {
+  echo "--file [csv file name] – this is the name of the CSV to be parsed" . "\n";
+
+  echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)" . "\n";
+
+  echo "--dry_run – this will be used with the --file directive in case we want to run the script but not insert
+  into the DB. All other functions will be executed, but the database won't be altered" . "\n";
+
+  echo "-u – MySQL username" . "\n";
+
+  echo "-p – MySQL password" . "\n";
+
+  echo "-h – MySQL host" . "\n";
+
+  echo "\r\n";
+
+  exit(1);
+}
+
 if(!isset($options["file"]) || empty(trim($options["file"])) ) {
     echo "Please provide a file path for csv file";
     return;
@@ -35,39 +54,59 @@ if(!isset($options["h"]) || empty(trim($options["h"])) ) {
     return;
 }
 
-if($help) {
-    echo "\r\n";
-
-    echo "--file [csv file name] – this is the name of the CSV to be parsed" . "\n";
-
-    echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)" . "\n";
-
-    echo "--dry_run – this will be used with the --file directive in case we want to run the script but not insert
-    into the DB. All other functions will be executed, but the database won't be altered" . "\n";
-
-    echo "-u – MySQL username" . "\n";
-
-    echo "-p – MySQL password" . "\n";
-
-    echo "-h – MySQL host" . "\n";
-
-    echo "\r\n";
-
-    exit(1);
-}
-
 var_dump($options);
 
 $dbInstance = DatabaseConnect::getInstance( $options["h"], $options["u"], $options["p"], "users_csv_upload");
 $db = $dbInstance->getConnection();
 
-var_dump($db);
-
 if($createTable) {
-    echo "Create table fresh";
+  // Attempt create table query execution
+  $sql = "CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
+    email VARCHAR(70) NOT NULL UNIQUE
+  )
+  ";
+  if($db->query($sql) === true){
+    echo "Table created successfully\n";
+
+  } else {
+    $message = "ERROR: Could not able to execute $sql. " . $db->error;
+    fwrite(STDOUT, $message);
+  }
+}
+
+if(!$dryRun) {
+  $file = fopen($options["file"], "r");
+  fgetcsv($file);
+  while (($row = fgetcsv($file)) !== FALSE) {
+    $stmt = $db->prepare("INSERT INTO users (name,surname,email) VALUES (?,?,?)");
+
+    $nameLower = strtolower($row[0]);
+    $name = ucfirst($nameLower);
+    $surnameLower = strtolower($row[1]);
+    $surname = ucwords($surnameLower);
+    $email = strtolower($row[2]);
+
+    $validEmail = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+    if(!$validEmail) {
+      $message = $row[2] . "is a invalid email. Please fix and try again ";
+      fwrite(STDOUT, $message);
+      $stmt->close();
+      return;
+    }
+
+    $stmt->bind_param("sss", $name, $surname, $email);
+    $stmt->execute();
+  }
+  echo "Csv data inserted into db.";
 }
 
 if($dryRun) {
     echo "Dry run";
 }
+
+$db->close();
 ?>
