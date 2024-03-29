faf32477af988ce7a4840d6c3784be4c23df2a6b
Gary O Brien <garyobrieneu@gmail.com>
2024-03-29 22:23:54 +1100

    change to check divisible by 15

--- foobar.php
+++ foobar.php
@@ -2,7 +2,7 @@
 
 $output = "";
 for( $i = 1; $i <= 100; $i++ ) {
-    if($i % 3 == 0 && $i % 5 == 0) $output .= "foobar";
+    if($i % 15 == 0) $output .= "foobar";
     else if($i % 3 == 0) $output .= "foo";
     else if($i % 5 == 0) $output .= "bar";
     else {
