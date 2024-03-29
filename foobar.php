b16b5fc9321115a218d58120f8482dec283312b1
Gary O Brien <garyobrieneu@gmail.com>
2024-03-29 22:12:57 +1100

    clean up and add foobar.php

---
+++ foobar.php
@@ -0,0 +1,16 @@
+<?php
+
+$output = "";
+for( $i = 1; $i <= 100; $i++ ) {
+    if($i % 3 == 0 && $i % 5 == 0) $output .= "foobar";
+    else if($i % 3 == 0) $output .= "foo";
+    else if($i % 5 == 0) $output .= "bar";
+    else {
+        $output .= $i;
+    }
+
+    $output .= ($i < 100) ? ", " : "";
+}
+
+echo $output;
+?>
